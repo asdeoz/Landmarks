fa3c8f6b05eb47cf5ac3da0a0380771141241c33
Carles Garcia <asdeoz@gmail.com>
2021-03-09 19:24:34 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Carles Garcia Bercial on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
