d161112b6c31d1fe3a43510842e3c8b35895ea7f
Carles Garcia <asdeoz@gmail.com>
2021-03-16 22:30:36 -0700

    Added watchOS detail

---
+++ WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by Carles Garcia Bercial on 3/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchLandmarks Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks Extension
+//
+//  Created by Carles Garcia Bercial on 3/16/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
