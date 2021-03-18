61bb39d6afbfab3ec9b2de33f264df503c15ea9f
Carles Garcia <asdeoz@gmail.com>
2021-03-18 13:56:05 -0700

    Added mac target

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Carles Garcia Bercial on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
